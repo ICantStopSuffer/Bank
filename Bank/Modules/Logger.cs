cd528566cdf677056df575c120315b6b2a816b40
ICantStopSuffer <sasha.plevovskih@yandex.ru>
2025-03-14 13:47:31 +0500

    Priting statistics

--- Bank/Modules/Logger.cs
+++ Bank/Modules/Logger.cs
@@ -28,10 +28,10 @@ namespace Bank.Modules
 
         public static void logTransaction(BankAccount account0, BankAccount account1, double amount, valueType type) {
 
-            File.AppendAllText(transactionFile, $"{account0.id}, {account1.id}, {amount}, {type}");
+            File.AppendAllText(transactionFile, $"{account0.id}, {account1.id}, {amount}, {type}, {DateTime.Now}");
         }
 
-        public static string viewTransactions() {
+        public static string viewTransactions(valueType? valueType = null) {
             string view = "";
 
             string[] lines = File.ReadAllLines(transactionFile);
@@ -42,11 +42,22 @@ namespace Bank.Modules
                 string bankId1 = splitted[1];
                 double amount = Double.Parse(splitted[2]);
                 valueType type = (valueType)Int32.Parse(splitted[3]);
+                DateTime date = DateTime.Parse(splitted[4]);
 
-                view += $"from: {bankId0}\n to: {bankId1}\n amount: {amount}\n type: {Enum.GetName(typeof(valueType), type)}\n\n";
+                if (valueType != null && valueType != type) {
+                    continue;
+                }
+
+                view += $"from: {bankId0}\n to: {bankId1}\n amount: {amount}\n type: {Enum.GetName(typeof(valueType), type)}\nDate and time: {date} \n\n";
             }
 
             return view;
+        }
+
+        public static void printStatistics() {
+            Console.WriteLine("RUB: \n" + viewTransactions(valueType.RUB));
+            Console.WriteLine("\nUSD: \n" + viewTransactions(valueType.USD));
+            Console.WriteLine("\nEUR: \n" + viewTransactions(valueType.EUR));
         }
 
         public static Exception logException(Exception exception)
